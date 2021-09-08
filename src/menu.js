ee409a37ab1830f683165adf36466650a8746a2d
root <root@PORT202105-018.localdomain>
2021-09-08 11:54:26 +0200

    [update] portfolio

--- src/menu.js
+++ src/menu.js
@@ -9,63 +9,163 @@ const skills = document.getElementById("2")
 const project = document.getElementById("3")
 const contact = document.getElementById("4")
 const check = document.getElementById("mobile")
+const smallScreen = window.matchMedia("(max-width: 768px)").matches;
+var screen = window.innerWidth
 
-window.onresize = reportWindowSize;
+if (smallScreen) {
 
-function reportWindowSize() {
+    check.addEventListener('click' , function(){
+
+        
+
+        if (check.checked == true){
+            menu.style.display = "flex";
+            menu.style.background = "white";
+            menu.style.flexDirection = "column";
+            menu.style.borderBottomLeftRadius = "1em";
+            menu.style.borderBottomRightRadius = "1em";
+            logo.style.borderBottomLeftRadius = "0";
+            logo.style.borderBottomRightRadius = "0";
+
+            function reportWindowSize() {
+
+                var screenObj = window.innerWidth;
+ 
+                if (screenObj > 768){
+ 
+                     menu.style.display = "flex";
+                     menu.style.flexDirection = "row";
+                } else {
+
+                    check.checked = false;
+                    menu.style.display = "none";
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+
+                }
+             }    
+ 
+             window.onresize = reportWindowSize;
+                
+        } else if (check.checked == false) {
+
+            menu.style.display = "none";
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+            
+            function reportWindowSize() {
+
+               var screenObj = window.innerWidth;
+
+               if (screenObj > 768){
+
+                    menu.style.display = "flex";
+                    menu.style.flexDirection = "row";
+               } else {
+                    menu.style.display = "none";
+               }
+            }    
+
+            window.onresize = reportWindowSize;
+
+           
+        }
+
+        presentation.onclick = function(){
+            menu.style.display = "none";
+            check.checked = false;
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+        }
     
-    const screenObj = window.innerWidth;
+        skills.onclick = function(){
+            menu.style.display = "none";
+            check.checked = false;
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+        }
+    
+        project.onclick = function(){
+            menu.style.display = "none";
+            check.checked = false;
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+        }
+    
+        contact.onclick = function(){
+            menu.style.display = "none";
+            check.checked = false;
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+        }
+    })    
+} else {
 
-    if ( screenObj <= 768){
+    function reportWindowSize() {
 
-        menu.style.display = "none";
-
-        check.addEventListener('click' , function(){
-
-            presentation.onclick = function(){
-                menu.style.display = "none";
-                check.checked = false;
-                logo.style.borderBottomLeftRadius = "1em";
-                logo.style.borderBottomRightRadius = "1em";
-            }
+       var screenObj = window.innerWidth;
         
-            skills.onclick = function(){
-                menu.style.display = "none";
-                check.checked = false;
-                logo.style.borderBottomLeftRadius = "1em";
-                logo.style.borderBottomRightRadius = "1em";
-            }
-        
-            project.onclick = function(){
-                menu.style.display = "none";
-                check.checked = false;
-                logo.style.borderBottomLeftRadius = "1em";
-                logo.style.borderBottomRightRadius = "1em";
-            }
-        
-            contact.onclick = function(){
-                menu.style.display = "none";
-                check.checked = false;
-                logo.style.borderBottomLeftRadius = "1em";
-                logo.style.borderBottomRightRadius = "1em";
-            }
+        if (screenObj <= 768){
 
-            if (check.checked == true){
-                menu.style.display = "flex";
-                menu.style.background = "white";
-                menu.style.flexDirection = "column";
-                menu.style.borderBottomLeftRadius = "1em";
-                menu.style.borderBottomRightRadius = "1em";
-                logo.style.borderBottomLeftRadius = "0";
-                logo.style.borderBottomRightRadius = "0";        
-            } else if (check.checked == false) {
-                menu.style.display = "none";
-                logo.style.borderBottomLeftRadius = "1em";
-                logo.style.borderBottomRightRadius = "1em";
-            }
-        }) 
-    } else {
-        menu.style.display = "flex";
-        menu.style.flexDirection = "row";
-    }
+            menu.style.display = "none";
+            logo.style.borderBottomLeftRadius = "1em";
+            logo.style.borderBottomRightRadius = "1em";
+
+            check.addEventListener('click' , function(){
+
+                
+
+                if (check.checked == true){
+                    menu.style.display = "flex";
+                    menu.style.background = "white";
+                    menu.style.flexDirection = "column";
+                    menu.style.borderBottomLeftRadius = "1em";
+                    menu.style.borderBottomRightRadius = "1em";
+                    logo.style.borderBottomLeftRadius = "0";
+                    logo.style.borderBottomRightRadius = "0";
+                        
+                } else if (check.checked == false) {
+        
+                    menu.style.display = "none";
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+                }
+                presentation.onclick = function(){
+                    menu.style.display = "none";
+                    check.checked = false;
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+                }
+            
+                skills.onclick = function(){
+                    menu.style.display = "none";
+                    check.checked = false;
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+                }
+            
+                project.onclick = function(){
+                    menu.style.display = "none";
+                    check.checked = false;
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+                }
+            
+                contact.onclick = function(){
+                    menu.style.display = "none";
+                    check.checked = false;
+                    logo.style.borderBottomLeftRadius = "1em";
+                    logo.style.borderBottomRightRadius = "1em";
+                }
+            })    
+        
+        } else {
+            check.checked = false;
+            menu.style.display = "flex";
+            menu.style.flexDirection = "row";
+        }
+
+    }    
+
+    window.onresize = reportWindowSize;
 }
